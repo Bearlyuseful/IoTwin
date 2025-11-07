d6c61a918509a4add824702d0236e9566bac95d4
Christian Fleischmann <bearlyuseful@gmail.com>
2025-11-07 07:30:19 +0100

    Added Socket functions

---
+++ src/common/socketClient.c
@@ -0,0 +1,31 @@
+#include "socketClient.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+int create_socket(const char* ip, uint16_t port){
+  int sock = socket(AF_INET, SOCK_STREAM, 0);
+  if(sock == -1){
+    perror("Socket creation failed.");
+    return -1;
+  }
+
+  struct sockaddr_in address;
+  memset(&address, 0, sizeof(address));
+  address.sin_family = AF_INET;
+  address.sin_port = htons(port);
+  inet_pton(AF_INET, ip, &address.sin_addr);
+
+  if(connect(sock, (struct sockaddr*)&address, sizeof(address)) < 0){
+    perror("Connection failed!");
+    close(sock);
+    return -1;
+  }
+  return sock;
+}
+
---
+++ src/common/socketClient.h
@@ -0,0 +1,22 @@
+#ifndef SOCKET_CLIENT_H
+#define SOCKET_CLIENT_H
+
+#include <pthread.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <sys/types.h>
+
+typedef struct {
+  int socket_fd;
+  pthread_t receive_thread;
+  pthread_t send_thread;
+  pthread_t logic_thread;
+} simulator_t;
+
+int create_socket(const char* ip, uint16_t port);
+int connect_simulator(simulator_t* sim);
+
+
+
+#endif
+
