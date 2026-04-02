91cb02547cb87676d9a7de58b121d8c28c46095b
Christian Fleischmann <Christian.Fleischmann@proton.me>
2026-04-02 16:43:55 +0200

    Feat can_bus: Implement ring buffer with send/receive

--- src/simulators/can_sim/can_bus.c
+++ src/simulators/can_sim/can_bus.c
@@ -1,12 +1,48 @@
 #include "can_bus.h"
 #include "../../common/log.h"
 
-bool can_init(void) {
+static CanFrame buffer[CAN_BUS_BUFFER_SIZE];
+static uint8_t head = 0;
+static uint8_t tail = 0;
+static uint8_t count = 0;
+
+bool can_bus_init(void) {
+  head = 0;
+  tail = 0;
+  count = 0;
   log_info("CAN bus initialized");
   return true;
 }
 
-bool can_send(uint32_t id, uint8_t data) {
-  printf("[CAN] Sending ID: %u Data: %u\n", id, data);
+bool can_bus_send(const CanFrame* frame) {
+  if(count >= CAN_BUS_BUFFER_SIZE){
+    log_info("Buffer is full");
+    return false;
+  }
+  buffer[tail] = *frame;
+  tail = (tail + 1) % CAN_BUS_BUFFER_SIZE;
+  count++;
+  log_info("CAN Message put in Buffer");
   return true;
+}
+
+
+bool can_bus_receive(CanFrame* frame) {
+
+  if(count == 0){
+    log_info("Buffer is empty");
+    return false;
+  }
+  *frame = buffer[head];
+  head = (head + 1) % CAN_BUS_BUFFER_SIZE;
+  count--;
+  log_info("CAN Message taken from Buffer");
+  return true;
+}
+
+void can_bus_destroy(){
+  head  = 0;
+  tail  = 0;
+  count = 0;
+  log_info("CAN bus destroyed");
 }
