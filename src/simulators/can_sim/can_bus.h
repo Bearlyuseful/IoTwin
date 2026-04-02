19b1530509efa82ced66284852d85d3bdc276399
Christian Fleischmann <Christian.Fleischmann@proton.me>
2026-04-02 16:49:22 +0200

    Revert "Refactor can_bus: update to use CanFrame"
    This reverts commit 23d7fd56ab8cfa91e7c9cf9f3bf9770882665670.

--- src/simulators/can_sim/can_bus.h
+++ src/simulators/can_sim/can_bus.h
@@ -2,11 +2,7 @@
 
 #include <stdbool.h>
 #include <stdint.h>
-#include "can_frame.h"
 
-#define CAN_BUS_BUFFER_SIZE 16
 
-bool can_bus_init(void);
-bool can_bus_send(const CanFrame* frame);
-bool can_bus_receive(CanFrame* frame);
-void can_bus_destroy(void);
+bool can_init(void);
+bool can_send(uint32_t id, uint8_t data);
